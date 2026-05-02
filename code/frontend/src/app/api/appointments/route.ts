5382af103156aec64430dd371c3dd7baf5cbdb42
AnushkaMR <e22323@eng.pdn.ac.lk>
2026-05-02 23:57:02 +0530

    modified:   code/frontend/src/app/api/appointments/route.ts

--- code/frontend/src/app/api/appointments/route.ts
+++ code/frontend/src/app/api/appointments/route.ts
@@ -1,5 +1,5 @@
 import { NextResponse } from 'next/server';
-import connectToDatabase from '@/lib/db';
+import connectToDatabase from '../../../lib/db';
 import Appointment from '@/models/Appointment';
 import { getServerSession } from 'next-auth/next';
 import { authOptions } from '@/lib/auth';
